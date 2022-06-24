4c5f76ca433ffa4cbb8567a2d47a3ffb8d72ebdd
Vinoth118 <vinoth@proisc.com>
2022-06-24 20:30:55 +0530

    made fant family dynamic

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,8 +1,25 @@
 import { Flex } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 import Header from "./header";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-    return <Flex fontFamily={"dynamic-font"} direction={"column"} w = "100%">
+    const [font, setFont] = useState<string | null>(null);
+    
+    useEffect(() => {
+        const initialFont = localStorage.getItem('dynamic-font');
+        if(initialFont) { setFont(initialFont) } else { setFont('sans-serif') }
+        window.addEventListener('storage', (event) => {
+            if(event.url.substring(0, 21) == 'http://localhost:3000' && event.key == "dynamic-font" && event.newValue != null) {
+                setFont(event.newValue)
+            }
+        })
+    }, [])
+
+    if(font == null) {
+        return <></>
+    }
+
+    return <Flex fontFamily={font} direction={"column"} w = "100%">
         <Header />
         <Flex direction={"column"} p = "30px">
             {children}
--- pages/page_1.tsx
+++ pages/page_1.tsx
@@ -2,23 +2,52 @@ import { Flex, Text } from "@chakra-ui/react";
 import { GetServerSideProps } from "next";
 import { useEffect } from "react";
 
+type fontType = "GOOGLE" | "CUSTOM" | "SELECTED";
+interface fontData { type: fontType, fontUrl: string, fontFamily: string } 
+
+const googleFontMockData: fontData = {
+    type: "GOOGLE",
+    fontUrl: "https://fonts.googleapis.com/css2?family=Rubik+Moonrocks&display=swap",
+    fontFamily: "Rubik Moonrocks"
+}
+
+const customFontMockData: fontData = {
+    type: "CUSTOM",
+    fontUrl: "/Calistha.ttf",
+    fontFamily: "Calistha"
+}
+
 export const getServerSideProps: GetServerSideProps = async (context) => {
+    const data: { font: fontData } = {
+        font: customFontMockData
+    }
     return {
-        props: {
-            font: "/Calistha.ttf"
-        }
+        props: data
     }
 }
 
-const Page1 = ({ font }: { font: string }) => {
+const Page1 = ({ font }: { font: fontData }) => {
+
     useEffect(() => {
-        const loadFont = async () => {
-            const fontFace = new FontFace('dynamic-font', `url(${font})`);
-            (document as any).fonts.add(fontFace);
-            await fontFace.load();
+        if(font.type == "GOOGLE") {
+            const linkElement = document.createElement('link');
+            linkElement.rel = 'stylesheet'
+            linkElement.id = 'google-font'
+            document.head.appendChild(linkElement)
+            linkElement.href = font.fontUrl;
+            localStorage.setItem('dynamic-font', font.fontFamily);
         }
-        loadFont();
+        if(font.type == "CUSTOM") {
+            const loadFont = async () => {
+                const fontFace = new FontFace(font.fontFamily, `url(${font.fontUrl})`);
+                (document as any).fonts.add(fontFace);
+                await fontFace.load();
+                localStorage.setItem('dynamic-font', font.fontFamily);
+            }
+            loadFont();
+        }
     }, [])
+    
     return (
         <Flex direction={'column'}>
             <Text fontWeight={"light"} fontSize={"14px"}>This is LIGHT_WEIGHT Text with font size of 14</Text>
